0bb4573afce37286123796b98a1550d359a1c10b
ashishrajput35 <ashishrajput35cs@gmail.com>
2025-07-07 01:45:18 +0530

    added the find all anagram in string problem

--- slidingwindow/FindAllAnagramsInString.java
+++ slidingwindow/FindAllAnagramsInString.java
@@ -1,4 +1,65 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class FindAllAnagramsInString {
+
+    public static void main(String[] args){
+
+        System.out.println("find all anagrams in the given string - ");
+        String s = "abab";
+        String p = "ab";
+
+        System.out.println(findAnagrams(s,p));
+    }
+
+
+
+
+
+    public static List<Integer> findAnagrams(String s,String p){
+
+        int pLength = p.length();
+        int sLength = s.length();
+
+        if(pLength> sLength)
+            return Collections.emptyList();
+
+        List<Integer> resList = new ArrayList<>();
+
+        int[] countP = new int[26];
+
+        int[] countS = new int[26];
+
+        for(int i = 0; i < pLength ; i++){
+
+            countP[p.charAt(i)-'a']++;
+            countS[s.charAt(i)-'a']++;
+
+        }
+
+        if(Arrays.equals(countP,countS)){
+            resList.add(0);
+        }
+
+        for(int right = pLength; right < sLength ; right++){
+
+            countS[s.charAt(right)-'a']++;
+            countS[s.charAt(right-pLength) -'a']--;
+
+            if(Arrays.equals(countP,countS))
+                resList.add(right-pLength+1);
+
+        }
+
+      return resList;
+    }
+
+
+
+
+
 
 
 }
