144cf978bd71a391d5a0b562203b64b686051258
ashishrajput35 <ashishrajput35cs@gmail.com>
2025-07-18 23:57:44 +0530

    leet code problem

--- Intervals/MinArrowShots.java
+++ Intervals/MinArrowShots.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class MinArrowShots {
 
     public int findMinArrowShots(int[][] points) {
--- matrix/GameOfLifeUpdate.java
+++ matrix/GameOfLifeUpdate.java
@@ -1,4 +1,53 @@
 public class GameOfLifeUpdate {
 
+    public void gameOfLife(int[][] board) {
 
+        int m = board.length, n = board[0].length;
+
+
+        int[][] directions = {
+                {-1, -1}, {-1, 0}, {-1, 1},
+                { 0, -1},          { 0, 1},
+                { 1, -1}, { 1, 0}, { 1, 1}
+        };
+
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int liveNeighbors = 0;
+
+                for (int[] dir : directions) {
+                    int ni = i + dir[0];
+                    int nj = j + dir[1];
+                    if (ni >= 0 && ni < m && nj >= 0 && nj < n) {
+                        if (board[ni][nj] == 1 || board[ni][nj] == -1) {
+                            liveNeighbors++;
+                        }
+                    }
+                }
+
+
+                if (board[i][j] == 1 && (liveNeighbors < 2 || liveNeighbors > 3)) {
+                    board[i][j] = -1;
+                }
+
+
+                if (board[i][j] == 0 && liveNeighbors == 3) {
+                    board[i][j] = 2;
+                }
+            }
+        }
+
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] > 0) {
+                    board[i][j] = 1;
+                } else {
+                    board[i][j] = 0;
+                }
+            }
+        }
+
+    }
 }
